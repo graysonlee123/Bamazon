023075fb487b84644fc48c46e2fc111780a47bd4
Grayson Gantek <graysonleegantek@gmail.com>
2019-08-21 17:32:17 -0400

    baisc sequelize product model setup and added .sql commands

---
+++ models/Product.js
@@ -0,0 +1,24 @@
+//Require Sequelize package to parse data types
+const dataType = require("sequelize");
+//Reauire database connection
+const sequelize = require("../config/config.js");
+
+//Make a new sequelize model
+const Product = sequelize.define("Product", {
+    product_name: {
+        type: dataType.STRING,
+        allowNull: false;
+    },
+    department_name: {
+        type: dataType.STRING,
+        allowNull: false
+    },
+    price: {
+        type: dataType.INTEGER,
+        allowNull: false
+    },
+    stock_quantity: {
+        type: dataType.INTEGER,
+        allowNull: false
+    }
+});
